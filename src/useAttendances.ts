14d63a80f8d5ac1969a10edd49cf0a7ef83a4657
Otsuki Hitoshi <hitochan777@gmail.com>
2020-07-21 04:34:09 +0900

    Add delete attendance feature

--- src/useAttendances.ts
+++ src/useAttendances.ts
@@ -1,7 +1,8 @@
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 
 import { Attendance } from "./attendance";
 import { fetcher } from "./fetcher";
+import { useState } from "react";
 
 export function useAttendances(
   userId: string | undefined,
@@ -25,4 +26,41 @@ export function useAttendances(
     isLoading: !error && !data,
     isError: error,
   };
+}
+
+export function useDeleteAttendance(): {
+  deleteAttendance: (
+    userId: string | undefined,
+    attendance: Attendance
+  ) => Promise<void>;
+  loading: boolean;
+  error: Error | null;
+} {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const deleteAttendance = async (
+    userId: string | undefined,
+    attendance: Attendance
+  ) => {
+    setLoading(true);
+    const endpoint = `${process.env.REACT_APP_API_ENDPOINT}/api/${userId}/attendance/${attendance.id}?code=${process.env.REACT_APP_API_KEY}`;
+    try {
+      await fetch(endpoint, {
+        method: "DELETE",
+      });
+    } catch (e) {
+      setError(e as Error);
+    } finally {
+      setLoading(false);
+      mutate(
+        `${process.env.REACT_APP_API_ENDPOINT}/api/${userId}/attendances?code=${
+          process.env.REACT_APP_API_KEY
+        }&clientId=attendance-taking-app&year=${attendance.occurredAt.getFullYear()}&month=${
+          attendance.occurredAt.getMonth() + 1
+        }`
+      );
+    }
+  };
+  return { deleteAttendance, loading, error };
 }
