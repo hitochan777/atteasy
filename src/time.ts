a96a902222dfb7f687c9493efecaccaf2c4fdaaa
Otsuki Hitoshi <hitochan777@gmail.com>
2020-07-26 16:07:22 +0900

    Refactor getTimePArt

--- src/time.ts
+++ src/time.ts
@@ -1,3 +1,8 @@
-export function getTimePart(date: Date): string {
-  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+export function getTimePart(date: Date, withColon = true): string {
+  const hourStr = `${date.getHours()}`;
+  const minStr= `${date.getMinutes()}`;
+  const secStr = `${date.getSeconds()}`;
+  const timeArray = [hourStr, minStr, secStr];
+  const delimiter = withColon ? ":" : "";
+  return timeArray.map(timeStr => timeStr.padStart(2, "0")).join(delimiter);
 }
